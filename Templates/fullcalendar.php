0c5453096d43d5956be72822e36070882c0651d5
Mason Davenport <masondaven@gmail.com>
2015-10-03 07:36:12 -0400

    fixed calendar

--- Templates/fullcalendar.php
+++ Templates/fullcalendar.php
@@ -259,27 +259,12 @@ session_start();
 </head>
 <body>
 <div class='wrapper'>
-    <nav class="navbar navbar-default navbar-static-top" style="height:40px;background-color:#002D62;">
-        <div class="container-fluid">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navigation-example-2" style="padding-top:0px;color:white;">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span style="color:white;" class="icon-bar"></span>
-                    <span style="color:white;" class="icon-bar"></span>
-                    <span style="color:white;" class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="index.php" style="padding-top:10px;color:white;">Welcome <?php echo $_SESSION['first_name'] . ' '. $_SESSION['last_name'];?></a>
-            </div>
-            <div class="collapse navbar-collapse">
-                <ul class="nav navbar-nav navbar-left">
-                    <!-- This is where other links for the slideout menu can go -->
-                </ul>
-            </div>
-        </div>
-    </nav>
+      <?php include('../includes/sidebar.php');?>
     <div class="main-panel">
-        <?php include('../includes/sidebar.php');?>
-        <div id='calendar'></div>
+        <?php include('../includes/navbar.php');?>
+        <div class="content">
+          <div id='calendar'></div>
+        </div>
     </div>
 
 
@@ -287,5 +272,24 @@ session_start();
 
 <div style='clear:both'></div>
 </div>
+
+<!--  Checkbox, Radio & Switch Plugins -->
+<script src="../assets/js/bootstrap-checkbox-radio-switch.js"></script>
+
+<!--  Charts Plugin -->
+<script src="../assets/js/chartist.min.js"></script>
+
+<!--  Notifications Plugin    -->
+<script src="../assets/js/bootstrap-notify.js"></script>
+
+<!--  Google Maps Plugin    -->
+<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?sensor=false"></script>
+
+<!-- Light Bootstrap Table Core javascript and methods for Demo purpose -->
+<script src="../assets/js/light-bootstrap-dashboard.js"></script>
+
+<!-- Light Bootstrap Table DEMO methods, don't include it in your project! -->
+<script src="../assets/js/demo.js"></script>
+
 </body>
 </html>
