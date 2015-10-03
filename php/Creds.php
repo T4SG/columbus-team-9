c93a9223d6a0b421a0f57fe8678656a5be67f650
Montana <montanawong@gmail.com>
2015-10-03 00:25:19 -0400

    fixed db issues

--- php/Creds.php
+++ php/Creds.php
@@ -5,3 +5,14 @@
  * Date: 10/2/15
  * Time: 22:05
  */
+
+//DB Creds
+define('DB_HOST', 'ec2-184-73-26-155.compute-1.amazonaws.com');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'code4good');
+define('DB_TYPE', 'mysql');
+define('DB_NAME', 'phpmyadmin');
+define('DB_PORT','3306');
+
+
+?>
--- php/functions.php
+++ php/functions.php
@@ -9,9 +9,19 @@
 require_once('Creds.php');
 
 //testing function
-public function getStudentInfo($student_id){
-    $pdo = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USERNAME, DB_PASSWORD);
-    $query = "SELECT * FROM students WHERE student_id = ?;";
+function getStudentInfo($student_id){
+    $dsn = 'mysql:host=127.0.0.1:3307;dbname=' . DB_NAME;
+    //return $dsn;
+    //return DB_NAME . ' ' . DB_USERNAME . ' ' . DB_PASSWORD;
+    try {
+        $pdo = new PDO($dsn,DB_USERNAME,DB_PASSWORD);
+    }
+    catch (PDOException $pe){
+        return $pe->getTraceAsString();
+    }
+
+    //pdo = new PDO('mysql:host=' . DB_HOST . ';port=' . DB_PORT . ';dbname=' . DB_NAME, DB_USERNAME, DB_PASSWORD);
+    $query = "SELECT * FROM student WHERE student_id = ?;";
     $stmt = $pdo->prepare($query);
     $stmt->bindParam(1, $student_id, PDO::PARAM_STR);
     if($stmt->execute()){
@@ -20,7 +30,7 @@ public function getStudentInfo($student_id){
     }
     else{
         //temp
-        return false;
+        return "";
     }
 
 }
