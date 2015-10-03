2bfb5668169980ce6b70570e9b2c26ee527c133a
Mohit Bhatia <mohitbhatia1994@gmail.com>
2015-10-03 05:42:32 -0400

    Update index.php

--- Templates/index.php
+++ Templates/index.php
@@ -35,7 +35,7 @@ include('../includes/header.php');
                   <img src="../assets/img/premium1.png" style="width:100px;height:100px;">
                 </div>
               </a>
-              <a href="calendar.html">
+              <a href="fullcalendar.html">
                 <div class="menu-box" id="bottom-right">
                   <p class="menu-title">CALENDER</p>
                   <img src="../assets/img/calendar146.png" style="width:100px;height:100px;">
