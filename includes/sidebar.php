4caab5152c8609d9d4e7b95a0ea01268aebfef4c
Mohit Bhatia <mohitbhatia1994@gmail.com>
2015-10-03 10:58:26 -0400

    Update sidebar.php

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -10,7 +10,7 @@
     <div class="sidebar-wrapper">
         <div class="logo">
             <a href="index.php" class="simple-text">
-                App Name
+                Pocket Pal
             </a>
         </div>
 
