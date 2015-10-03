9244b79ef416b90f79e805e14ce328fdc9077e89
Mohit Bhatia <mohitbhatia1994@gmail.com>
2015-10-03 05:22:26 -0400

    Update sidebar.php

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -9,7 +9,7 @@
 
     <div class="sidebar-wrapper">
         <div class="logo">
-            <a href="http://www.creative-tim.com" class="simple-text">
+            <a href="index.php" class="simple-text">
                 App Name
             </a>
         </div>
