81ee2fb72cbaa94a0ff4d3428586d796fef665ad
Montana <montanawong@gmail.com>
2015-10-03 05:14:15 -0400

    added includes

--- Templates/profile.php
+++ Templates/profile.php
@@ -2,101 +2,14 @@
 session_start();
 include_once('../php/functions.php');
 $student_id = $_SESSION['student_id'];
+include('../includes/header.php');
 ?>
 
-<!doctype html>
-<html lang="en">
-<head>
-	<meta charset="utf-8" />
-	<link rel="icon" type="image/png" href="../assets/img/favicon.ico">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 
-	<title>Light Bootstrap Dashboard by Creative Tim</title>
-
-	<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
-    <meta name="viewport" content="width=device-width" />
-    
-    
-    <!-- Bootstrap core CSS     -->
-    <link href="../assets/css/bootstrap.min.css" rel="stylesheet" />
-
-    <!-- Animation library for notifications   -->
-    <link href="../assets/css/animate.min.css" rel="stylesheet"/>
-    
-    <!--  Light Bootstrap Table core CSS    -->
-    <link href="../assets/css/light-bootstrap-dashboard.css" rel="stylesheet"/>
-    
-    
-    <!--  CSS for Demo Purpose, don't include it in your project     -->
-    <link href="../assets/css/demo.css" rel="stylesheet" />
-    
-        
-    <!--     Fonts and icons     -->
-    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
-    <link href='http://fonts.googleapis.com/css?family=Roboto:400,700,300' rel='stylesheet' type='text/css'>
-    <link href="../assets/css/pe-icon-7-stroke.css" rel="stylesheet" />
-</head>
 <body> 
 
 <div class="wrapper">
-    <div class="sidebar" data-color="purple" data-image="assets/img/sidebar-5.jpg">    
-    
-    <!--   you can change the color of the sidebar using: data-color="blue | azure | green | orange | red | purple" -->
-    
-    
-    	<div class="sidebar-wrapper">
-            <div class="logo">
-                <a href="http://www.creative-tim.com" class="simple-text">
-                    <?php echo $_SESSION['first_name'] . ' ' . $_SESSION['last_name'];?>
-                </a>
-            </div>
-                       
-            <ul class="nav">
-                <li>
-                    <a href="dashboard.html">
-                        <i class="pe-7s-graph"></i> 
-                        <p>Dashboard</p>
-                    </a>            
-                </li>
-                <li class="active">
-                    <a href="user.html">
-                        <i class="pe-7s-user"></i> 
-                        <p>User Profile</p>
-                    </a>
-                </li> 
-                <li>
-                    <a href="table.html">
-                        <i class="pe-7s-note2"></i> 
-                        <p>Table List</p>
-                    </a>        
-                </li>
-                <li>
-                    <a href="typography.html">
-                        <i class="pe-7s-news-paper"></i> 
-                        <p>Typography</p>
-                    </a>        
-                </li>
-                <li>
-                    <a href="icons.html">
-                        <i class="pe-7s-science"></i> 
-                        <p>Icons</p>
-                    </a>        
-                </li>
-                <li>
-                    <a href="maps.html">
-                        <i class="pe-7s-map-marker"></i> 
-                        <p>Maps</p>
-                    </a>        
-                </li>
-                 <li>
-                    <a href="notifications.html">
-                        <i class="pe-7s-bell"></i> 
-                        <p>Notifications</p>
-                    </a>        
-                </li>
-            </ul> 
-    	</div>
-    </div>
+    <?php include('../includes/sidebar.php');?>
     
     <div class="main-panel">
         <nav class="navbar navbar-default navbar-fixed">
