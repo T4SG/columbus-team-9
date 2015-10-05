0cbd66395f3614a8c42685d7c94735856848a484
Mohit Bhatia <mohitbhatia1994@gmail.com>
2015-10-05 00:40:00 -0400

    Update sidebar.php

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,12 +1,5 @@
 <div class="sidebar" data-color="orange" data-image="../assets/img/sidebar-5.jpg">
 
-    <!--
-
-        Tip 1: you can change the color of the sidebar using: data-color="blue | azure | green | orange | red | purple"
-        Tip 2: you can also add an image using data-image tag
-
-    -->
-
     <div class="sidebar-wrapper">
         <div class="logo">
             <a href="index.php" class="simple-text">
